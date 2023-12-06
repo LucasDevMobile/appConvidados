2476bd5734d8acbcf8ec57b86e8baadd2ade941a
Lucas Oliveira <lucas.devmobile@gmail.com>
2023-12-06 12:39:57 -0300

    binding pronto

--- app/src/main/java/com/example/convidados/GuestFormMainActivity.kt
+++ app/src/main/java/com/example/convidados/GuestFormMainActivity.kt
@@ -1,11 +1,35 @@
 package com.example.convidados
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import com.example.convidados.databinding.ActivityGuestFormBinding
 
-class GuestFormActivity : AppCompatActivity() {
+class GuestFormActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityGuestFormBinding
+    private lateinit var viewModel: GuestFormViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_guest_form)
+
+        binding = ActivityGuestFormBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        viewModel = ViewModelProvider(this).get(GuestFormViewModel::class.java)
+
+
+        binding.buttonSave.setOnClickListener(this)
+        binding
+            .radioPresent.isChecked = true
+
+    }
+
+    override fun onClick(v: View) {
+
+        if (v.id == R.id.button_save) {
+
+
+        }
     }
 }
---
+++ app/src/main/java/com/example/convidados/GuestFormViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.convidados
+
+import androidx.lifecycle.ViewModel
+
+class GuestFormViewModel: ViewModel() {
+
+
+}
