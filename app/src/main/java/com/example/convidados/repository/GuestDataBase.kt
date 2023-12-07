d7e2177202cd5010c416006934fd1a4b24ac9a87
Lucas oliveira <lucas.mobiledev@gmail.com>
2023-12-06 23:35:23 -0300

    data base created

---
+++ app/src/main/java/com/example/convidados/repository/GuestDataBase.kt
@@ -0,0 +1,29 @@
+package com.example.convidados.repository
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class GuestDataBase(context: Context) : SQLiteOpenHelper(context, NAME, null, VERSION) {
+
+    companion object {
+
+        private const val NAME = "guestdb"
+        private const val VERSION = 1
+
+    }
+
+    override fun onCreate(db: SQLiteDatabase) {
+        db.execSQL(
+            "create table Guest(" +
+                    "id integer primary key autoincrement," +
+                    " name text," +
+                    "presence integer);")
+
+
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+
+    }
+}
